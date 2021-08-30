4c704eb8bd75f347b94a3ef1394cdaf7037c68a2
Bundy-Mundi <bennie.ben095@gmail.com>
2021-08-30 08:57:54 -0700

    Basic frontend set up

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
-console.log("hello");
-
+import path from "path";
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "/views"));
+app.use("/public", express.static(path.join(__dirname, "/public")));
 app.get("/", (req, res)=>{
-    res.send("Server is alive");
+    res.render("home");
 });
 
 app.listen(3000, () => console.log("Listening On: http://localhost:3000"));
