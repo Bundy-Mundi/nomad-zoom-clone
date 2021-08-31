9ae76ceda686f7a13b415be0f629912dcacaba3a
Bundy-Mundi <bennie.ben095@gmail.com>
2021-08-31 10:55:06 -0700

    Add websocket server to express

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import path from "path";
+import http from "http";
+import ws from "ws";
 const app = express();
 
 app.set("view engine", "pug");
@@ -8,5 +10,6 @@ app.use("/public", express.static(path.join(__dirname, "/public")));
 app.get("/", (req, res)=>{
     res.render("home");
 });
-
-app.listen(3000, () => console.log("Listening On: http://localhost:3000"));
+const server = http.createServer(app);
+const wsServer = new ws.Server({server});
+server.listen(3000, () => console.log("Listening On: http://localhost:3000\nListening On: ws://localhost:3000"));
