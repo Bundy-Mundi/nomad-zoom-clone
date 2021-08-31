3b3ea2cc182ef57612a3a5508a65a2827abe1fc5
Bundy-Mundi <bennie.ben095@gmail.com>
2021-08-31 12:03:55 -0700

    Frontend chatlist compelted

--- src/server.js
+++ src/server.js
@@ -12,4 +12,18 @@ app.get("/", (req, res)=>{
 });
 const server = http.createServer(app);
 const wsServer = new ws.Server({server});
+const sockets = [];
+
+/* WebSocket */
+wsServer.on("connection", (socket) => {
+    sockets.push(socket);
+    console.log("Client connected.");
+    socket.on("close", () => console.log("Client Disconnected"));
+    socket.on("message", (message) => {
+        sockets.forEach(s => s.send(message));
+        console.log(message.toString());
+    })
+    socket.send("You are connected to the server.")
+});
+
 server.listen(3000, () => console.log("Listening On: http://localhost:3000\nListening On: ws://localhost:3000"));
