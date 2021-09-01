ec6eae6125a5db9e6e1af0958bd226f47b9cdb0e
Bundy-Mundi <bennie.ben095@gmail.com>
2021-08-31 19:32:57 -0700

    Adding socket.io

--- src/server.js
+++ src/server.js
@@ -1,21 +1,24 @@
 import express from "express";
 import path from "path";
 import http from "http";
-import ws from "ws";
+//import ws from "ws";
+import SocketIO from "socket.io";
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "/views"));
 app.use("/public", express.static(path.join(__dirname, "/public")));
-app.get("/", (req, res)=>{
-    res.render("home");
-});
+app.get("/", (req, res)=>res.render("home"));
+app.get("/*", (req, res)=>res.redirect("/"));
 const server = http.createServer(app);
-const wsServer = new ws.Server({server});
-const sockets = [];
+const io = SocketIO(server);
+io.on("connection", () => {
+    
+});
 
 /* WebSocket */
-wsServer.on("connection", (socket) => {
+//const wsServer = new ws.Server({server});
+/*wsServer.on("connection", (socket) => {
     sockets.push(socket);
     console.log("Client connected.");
     socket.on("close", () => console.log("Client Disconnected"));
@@ -24,6 +27,6 @@ wsServer.on("connection", (socket) => {
         console.log(message.toString());
     })
     socket.send("You are connected to the server.")
-});
+});*/
 
 server.listen(3000, () => console.log("Listening On: http://localhost:3000\nListening On: ws://localhost:3000"));
