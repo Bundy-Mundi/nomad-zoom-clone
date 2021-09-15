00dda4905091e69091ff15ed1fed15763fb72480
Bundy-Mundi <bennie.ben095@gmail.com>
2021-09-15 10:10:24 -0700

    #3.0 & #3.1 update

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,68 @@
 const socket = io();
 const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const camBtn = document.getElementById("cam");
+const camerasSelect = document.getElementById("cameras");
 let myStream;
-socket.on("server-connection", (msg) => console.log(msg));
-async function getMedia(constraints) {
+let isMuted = true;
+let isCamOn = true;
+// socket.on("server-connection", (msg) => console.log(msg));
+async function getCameras() {
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind === "videoinput");
+        
+        cameras.forEach(camera => {
+            let child = document.createElement("option");
+            child.innerText = camera.label;
+            child.value = camera.deviceId;
+            camerasSelect.appendChild(child);
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+async function getMedia() {
     try{
         myStream = await navigator.mediaDevices.getUserMedia({
             audio: true,
             video: true
         });
-        myFace.srcObject = myStream
+        myFace.srcObject = myStream;
+        myStream.getAudioTracks()
+            .forEach(track => (track.enabled = false));
+        myStream.getVideoTracks()
+            .forEach(track => (track.enabled = false));
     }catch(err){
         console.log(err);
     }
 }
 
 getMedia();
+getCameras();
+
+function handleMute (){
+    isMuted = !isMuted;
+    myStream.getAudioTracks()
+        .forEach(track => (track.enabled = !track.enabled));
+    if(isMuted) {
+        muteBtn.innerText = "Unmute";
+    }
+    else {
+        muteBtn.innerText = "Mute";
+    }
+}
+function handleCam (){
+    isCamOn = !isCamOn;
+    myStream.getVideoTracks()
+        .forEach(track => (track.enabled = !track.enabled));
+    if(isCamOn) {
+        camBtn.innerText = "Turn Camera Off";
+    }
+    else {
+        camBtn.innerText = "Turn Camera On";
+    }
+}
+console.log(isCamOn, isMuted)
+muteBtn.addEventListener("click", handleMute);
+camBtn.addEventListener("click", handleCam);
